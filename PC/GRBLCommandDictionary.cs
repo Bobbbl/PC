2281b796d1dffe7bc78ab3005388489969c40407
Draxinger, Sebastian (ELATEC D) <s.draxinger@elatec.com>
2019-09-06 23:40:02 +0200

    Added GetCommand

--- PC/GRBLCommandDictionary.cs
+++ PC/GRBLCommandDictionary.cs
@@ -10,12 +10,12 @@ namespace PC
         private Dictionary<GRBLCommand, string> _CommandString = new Dictionary<GRBLCommand, string>()
         {
             { GRBLCommand.GRBL_ViewSettings, "$$"},
-            {GRBLCommand.GRBL_WriteSetting, "&@="},
+            {GRBLCommand.GRBL_WriteSetting, "&@@=@@@"},
             {GRBLCommand.GRBL_ViewGCODEParameters, "$#"},
             {GRBLCommand.GRBL_ViewGCODEParserState, "$G"},
             {GRBLCommand.GRBL_ViewBuildInfo, "$I"},
             {GRBLCommand.GRBL_ViewStartUpBlocks, "$N"},
-            {GRBLCommand.GRBL_SaveStartUpBlock, "$N@@@="},
+            {GRBLCommand.GRBL_SaveStartUpBlock, "$N@@=@@@"},
             {GRBLCommand.GRBL_CheckGCODEMode, "$C"},
             {GRBLCommand.GRBL_KillAlarmLock, "$X"},
             {GRBLCommand.GRBL_RunHomincCycle, "$H"},
@@ -56,129 +56,20 @@ namespace PC
         }
 
 
-        public string GetCommand(GRBLCommand command)
+        public string GetCommand(GRBLCommand command, string bezeichner = "", string wert = "")
         {
-
             string r = string.Empty;
 
-            switch (command)
-            {
-                case GRBLCommand.GRBL_ViewSettings:
-                    r = "$$";
-                    break;
-                case GRBLCommand.GRBL_WriteSetting:
-                    // Hier muss dann @ in der entsprechenden Funktion ersetzt werden durch die gewünschte Option
-                    r = "&@=";
-                    break;
-                case GRBLCommand.GRBL_ViewGCODEParameters:
-                    r = "$#";
-                    break;
-                case GRBLCommand.GRBL_ViewGCODEParserState:
-                    r = "$G";
-                    break;
-                case GRBLCommand.GRBL_ViewBuildInfo:
-                    r = "$I";
-                    break;
-                case GRBLCommand.GRBL_ViewStartUpBlocks:
-                    r = "$N";
-                    break;
-                case GRBLCommand.GRBL_SaveStartUpBlock:
-                    // Hier muss dann @ in der entsprechenden Funktion ersetzt werden durch die gewünschte Line
-                    r = "$N@=";
-                    break;
-                case GRBLCommand.GRBL_CheckGCODEMode:
-                    r = "$C";
-                    break;
-                case GRBLCommand.GRBL_KillAlarmLock:
-                    r = "$X";
-                    break;
-                case GRBLCommand.GRBL_RunHomincCycle:
-                    r = "$H";
-                    break;
-                case GRBLCommand.GRBL_RunJoggingMogion:
-                    r = "$J=";
-                    break;
-                case GRBLCommand.GRBL_ResetGRBLSettings:
-                    r = "$RST=$";
-                    break;
-                case GRBLCommand.GRBL_ResetG54_G59Settings:
-                    r = "$RST=#";
-                    break;
-                case GRBLCommand.GRBL_ResetEEPROM:
-                    r = "$RST=*";
-                    break;
-                case GRBLCommand.GRBL_EnableSleepMode:
-                    r = "$SLP";
-                    break;
-                case GRBLCommand.RTC_SoftReset:
-                    r = Convert.ToString(0x18);
-                    break;
-                case GRBLCommand.RTC_StatusReportQuery:
-                    r = "?";
-                    break;
-                case GRBLCommand.RTC_CycleStart_Resume:
-                    r = "~";
-                    break;
-                case GRBLCommand.RTC_FeedHold:
-                    r = "!";
-                    break;
-                case GRBLCommand.RTC_SafetyDoor:
-                    r = Convert.ToString(0x84);
-                    break;
-                case GRBLCommand.RTC_JogCancel:
-                    r = Convert.ToString(0x85);
-                    break;
-                case GRBLCommand.RTC_SetFeed100:
-                    r = Convert.ToString(0x90);
-                    break;
-                case GRBLCommand.RTC_IncreaseFeedBy10Percent:
-                    r = Convert.ToString(0x91);
-                    break;
-                case GRBLCommand.RTC_IncreaseFeedBy1Percent:
-                    r = Convert.ToString(0x93);
-                    break;
-                case GRBLCommand.RTC_DecreaseFeedBy10Percent:
-                    r = Convert.ToString(0x92);
-                    break;
-                case GRBLCommand.RTC_DecreaseFeedBy1Percent:
-                    r = Convert.ToString(0x94);
-                    break;
-                case GRBLCommand.RTC_SetRapid100:
-                    r = Convert.ToString(0x95);
-                    break;
-                case GRBLCommand.RTC_SetRapid50Percent:
-                    r = Convert.ToString(0x96);
-                    break;
-                case GRBLCommand.RTCSetRapid25Percent:
-                    r = Convert.ToString(0x97);
-                    break;
-                case GRBLCommand.RTC_SetSpindle100:
-                    r = Convert.ToString(0x99);
-                    break;
-                case GRBLCommand.RTC_IncreaseSpindleBy10Percent:
-                    r = Convert.ToString(0x9A);
-                    break;
-                case GRBLCommand.RTC_IncreaseSpindleBy1Percent:
-                    r = Convert.ToString(0x9C);
-                    break;
-                case GRBLCommand.RTC_DecreaseSpindleBy10Percent:
-                    r = Convert.ToString(0x9B);
-                    break;
-                case GRBLCommand.RTC_DecreaseSpindleBy1Percent:
-                    r = Convert.ToString(0x9D);
-                    break;
-                case GRBLCommand.RTC_ToggleSpindleStop:
-                    r = Convert.ToString(0x9E);
-                    break;
-                case GRBLCommand.RTC_ToggleFloodCoolant:
-                    r = Convert.ToString(0xA0);
-                    break;
-                case GRBLCommand.RTC_ToggleMistCoolant:
-                    r = Convert.ToString(0xA1);
-                    break;
+            r = _CommandString[command];
 
-                default:
-                    break;
+            if(r.Contains("@@"))
+            {
+                r.Replace("@@", bezeichner);
+            }
+
+            if(r.Contains("@@@"))
+            {
+                r.Replace("@@@", wert);
             }
 
             return r;
