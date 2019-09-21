2ebcef38212aaf08bd34e8f509530d65b78ec3b2
Draxinger, Sebastian (ELATEC D) <s.draxinger@elatec.com>
2019-09-21 08:27:18 +0200

    PresentViewModel

---
+++ PC/PresentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace PC
+{
+    public class PresentViewModel
+    {
+        public List<string> ConsoleText { get; set; }
+
+        public double CurrentX { get; set; }
+
+        public double CurrentY { get; set; }
+
+        public double CurrentZ { get; set; }
+
+        public DispatcherTimer UpdateTimer { get; set; } = new DispatcherTimer();
+
+        public DispatcherTimer PollTimer { get; set; } = new DispatcherTimer();
+
+        public string SelectedPortName { get; set; }
+
+        public int SelectedBaudRate { get; set; }
+
+    }
+}
