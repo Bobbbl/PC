73bd247ffd4c311ea728654086b8862e7adf0af4
Draxinger, Sebastian (ELATEC D) <s.draxinger@elatec.com>
2019-10-04 16:08:45 +0200

    Search For Ports added

--- PC/ViewModels/PortViewModel.cs
+++ PC/ViewModels/PortViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace PC
 {
@@ -11,10 +13,35 @@ namespace PC
 
         public List<string> PortList { get; set; } = new List<string>();
 
-        public PortViewModel()
+
+        private DispatcherTimer PortSearchTimer = new DispatcherTimer();
+
+
+        private void SearchForPortAndAddOrRemove(object sender, EventArgs e)
         {
+            List<string> portlist = SerialPort.GetPortNames().ToList();
+
+            var oldports = PortList.Except(portlist).ToList();
+            var newports = portlist.Except(PortList).ToList();
+
+            if (oldports.Count == 0 && newports.Count == 0)
+            {
+                return;
+            }
+
+            PortList = portlist;
+            OnPropertyChanged(nameof(PortList));
+
 
         }
 
+
+        public PortViewModel()
+        {
+
+            PortSearchTimer.Tick += (sender, e) => { SearchForPortAndAddOrRemove(sender, e); };
+            PortSearchTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            PortSearchTimer.Start();
+        }
     }
 }
