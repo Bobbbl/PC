a2f2f694f67b0786367fd1e94cdcbddf9fc0d3c3
Draxinger, Sebastian (ELATEC D) <s.draxinger@elatec.com>
2019-11-20 09:16:52 +0100

    Added PlotViewModel

---
+++ PC/ViewModels/PlotViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PC
+{
+    class PlotViewModel : BaseViewModel
+    {
+        #region Static INotifyPropertyChanged
+
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+        public static void RaiseStaticPropertyChanged(string PropName)
+        {
+            EventHandler<PropertyChangedEventArgs> handler = StaticPropertyChanged;
+            if (handler != null)
+            {
+                PropertyChangedEventArgs e = new PropertyChangedEventArgs(PropName);
+                handler(typeof(ConsoleViewModel), e);
+            }
+        }
+
+        #endregion
+
+        
+
+    }
+}
