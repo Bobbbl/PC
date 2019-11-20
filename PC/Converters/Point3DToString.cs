549472931d74e003b920721002e2c73001e25d14
Draxinger, Sebastian (ELATEC D) <s.draxinger@elatec.com>
2019-11-20 10:20:45 +0100

    Added Point3DToString.cs

---
+++ PC/Converters/Point3DToString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media.Media3D;
+
+namespace PC
+{
+    public class Point3DToString : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Point3D p = PlotViewModel.WheelPosition;
+            string r = string.Empty;
+
+            r += p.X.ToString() + " ";
+            r += p.Y.ToString() + " ";
+            r += p.Z.ToString();
+
+            return r;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new Point3DToString();
+        }
+    }
+}
